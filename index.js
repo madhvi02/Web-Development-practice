8d4dd0beafe23af71dcfafa3f17abadfc1048bfd
madhvi02 <amdhvisuri2222@gmail.com>
2021-05-20 18:56:24 +0530

    controllers and routers

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express=require('express');
-const app=express();
+const app=express();  //run the express server 
 const port=8000;
+//setting up the router before the server starts
+const router=require('./routes/index');
+app.use('/',router);
+
+// app.get('/',function()
+// {
+
+// });
+
 
 app.listen(port,function(err)
 {
